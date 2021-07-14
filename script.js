3c924e6e85c5fd0e550c0d6fa5160c9d10f004c0
sblahov <sblahov@gmail.com>
2021-07-14 19:58:51 -0400

    refactored script into OOP

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const nav = document.querySelector(".nav");
 const navLinks = document.querySelector(".nav__navigation");
 const header = document.querySelector(".header");
@@ -10,101 +12,113 @@ const btnLeft2 = document.querySelector(".slider-2__btn--left");
 const btnRight2 = document.querySelector(".slider-2__btn--right");
 
 // Sticky Nav
-const navHeight = nav.getBoundingClientRect().height;
 
-const observerCallback = function (entries, observer) {
-  const [entry] = entries;
-  if (!entry.isIntersecting) nav.classList.add("nav__sticky");
-  if (entry.isIntersecting) nav.classList.remove("nav__sticky");
-};
+class Website {
+  constructor() {
+    // Slider
+    this._slider(slides1, btnLeft1, btnRight1);
+    this._slider(slides2, btnLeft2, btnRight2);
 
-const observerOptions = {
-  root: null,
-  threshold: 0.1,
-  rootMargin: `-${navHeight}px`,
-};
-
-const headerObserver = new IntersectionObserver(
-  observerCallback,
-  observerOptions
-);
-headerObserver.observe(header);
-
-// Smooth Scrolling
-navLinks.addEventListener("click", function (e) {
-  e.preventDefault();
-
-  if (!e.target.classList.contains("nav__link")) return;
-  if (e.target.classList.contains("nav__link")) {
-    const id = e.target.getAttribute("href");
-    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+    // Attach Event Handlers
+    navLinks.addEventListener("click", this._smoothScroll);
+    accordion.addEventListener("click", this._toggleAccordion);
   }
-});
+  _smoothScroll = function (e) {
+    e.preventDefault();
 
-// Expand and collapse services
-
-accordion.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (!e.target.classList.contains("accordion__button")) return;
-
-  if (e.target.classList.contains("accordion__button")) {
-    const button = e.target;
-    const detail = e.target.nextElementSibling;
-    const icon = button.querySelector(".svg__link");
-    if (detail.classList.contains("detail--collapsed")) {
-      detail.classList.remove("detail--collapsed");
-      icon.setAttribute(
-        "xlink:href",
-        "img/symbol-defs.svg#icon-chevron-thin-up"
-      );
-    } else {
-      detail.classList.add("detail--collapsed");
-      icon.setAttribute(
-        "xlink:href",
-        "img/symbol-defs.svg#icon-chevron-thin-down"
-      );
+    if (!e.target.classList.contains("nav__link")) return;
+    if (e.target.classList.contains("nav__link")) {
+      const id = e.target.getAttribute("href");
+      document.querySelector(id).scrollIntoView({ behavior: "smooth" });
     }
-  }
-});
+  };
 
-// Slider
+  _toggleAccordion = function (e) {
+    e.preventDefault();
+    if (!e.target.classList.contains("accordion__button")) return;
 
-const slider = function (slides, btnLeft, btnRight) {
-  console.log(slides);
+    if (e.target.classList.contains("accordion__button")) {
+      const button = e.target;
+      const detail = e.target.nextElementSibling;
+      const icon = button.querySelector(".svg__link");
+      if (detail.classList.contains("detail--collapsed")) {
+        detail.classList.remove("detail--collapsed");
+        icon.setAttribute(
+          "xlink:href",
+          "img/symbol-defs.svg#icon-chevron-thin-up"
+        );
+      } else {
+        detail.classList.add("detail--collapsed");
+        icon.setAttribute(
+          "xlink:href",
+          "img/symbol-defs.svg#icon-chevron-thin-down"
+        );
+      }
+    }
+  };
 
-  let curSlide = 0;
-  const maxSlide = slides.length;
+  _slider = function (slides, btnLeft, btnRight) {
+    let curSlide = 0;
+    const maxSlide = slides.length;
 
-  const goToSlide = function (slide) {
-    slides.forEach(
-      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    const goToSlide = function (slide) {
+      slides.forEach(
+        (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+      );
+    };
+
+    const nextSlide = function () {
+      if (curSlide === maxSlide - 1) {
+        curSlide = 0;
+      } else {
+        curSlide++;
+      }
+      goToSlide(curSlide);
+    };
+
+    const prevSlide = function () {
+      if (curSlide === 0) {
+        curSlide = maxSlide - 1;
+      } else {
+        curSlide--;
+      }
+
+      goToSlide(curSlide);
+    };
+
+    goToSlide(0);
+
+    btnLeft.addEventListener("click", prevSlide);
+    btnRight.addEventListener("click", nextSlide);
+  };
+}
+
+const website = new Website();
+
+///////////////////////////////////////////////////////////////////////////////
+
+class Observer {
+  constructor() {
+    const navHeight = nav.getBoundingClientRect().height;
+
+    const observerOptions = {
+      root: null,
+      threshold: 0.1,
+      rootMargin: `-${navHeight}px`,
+    };
+
+    const headerObserver = new IntersectionObserver(
+      this._observerCallback,
+      observerOptions
     );
+    headerObserver.observe(header);
+  }
+
+  _observerCallback = function (entries, observer) {
+    const [entry] = entries;
+    if (!entry.isIntersecting) nav.classList.add("nav__sticky");
+    if (entry.isIntersecting) nav.classList.remove("nav__sticky");
   };
+}
 
-  const nextSlide = function () {
-    if (curSlide === maxSlide - 1) {
-      curSlide = 0;
-    } else {
-      curSlide++;
-    }
-    goToSlide(curSlide);
-  };
-
-  const prevSlide = function () {
-    if (curSlide === 0) {
-      curSlide = maxSlide - 1;
-    } else {
-      curSlide--;
-    }
-
-    goToSlide(curSlide);
-  };
-
-  goToSlide(0);
-
-  btnLeft.addEventListener("click", prevSlide);
-  btnRight.addEventListener("click", nextSlide);
-};
-
-slider(slides1, btnLeft1, btnRight1);
-slider(slides2, btnLeft2, btnRight2);
+const observer = new Observer();
